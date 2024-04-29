74852bb103930093fab38b433a31276f0820b7e0
NurL <noers.ili@gmail.com>
2024-04-29 15:45:15 +0700

    creating splash page

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0XFF134736),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Image.asset(
+              'assets/logo.png',
+              height: 150,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
